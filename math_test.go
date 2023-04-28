69a0a2efe08665c16ceabf1576a825ba5ec1b9cf
Anderson <octobrain.it@gmail.com>
2023-04-28 08:58:43 -0300

    feat: Add soma function

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Soma result is invalid: Expected -> %d. Recieved -> %d", 30, total)
+		t.Errorf("Soma result is invalid: Expected -> %d | Recieved -> %d", 30, total)
 	}
 }
